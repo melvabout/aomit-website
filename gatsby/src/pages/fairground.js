f5e231292cbbee36ad7f0618fa095eefc1656d0c
Andrew Melvin <andy@aomit.co.uk>
2024-05-06 10:47:53 +0100

    Corrected the new links as they are html not markdown

--- gatsby/src/pages/fairground.js
+++ gatsby/src/pages/fairground.js
@@ -5,9 +5,9 @@ import Seo from '../components/seo'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Fairground">
-      <p>Think sand or playground and make it bigger.</p>
-      <p>The goal is to build a mini web application that makes use of [FastAPI](https://fastapi.tiangolo.com/) and is deployed on to a monitored Kubernetes cluster in AWS. Inspired by [Kubernetes the Hardway](https://github.com/kelseyhightower/kubernetes-the-hard-way), not everything will be the most efficient, but it will be more "fun".</p>
-      <p>First piece of the puzzle is to build the Kubernetes AMIs for this we need packer and ansible. The repo is [fairground-machine-images](https://github.com/melvabout/fairground-machine-images)</p>
+      <p>Think sand pit or playground and make it bigger.</p>
+      <p>The goal is to build a mini web application that makes use of <a href='https://fastapi.tiangolo.com/'>FastAPI</a> and is deployed on to a monitored Kubernetes cluster in AWS. Inspired by <a href='https://github.com/kelseyhightower/kubernetes-the-hard-way'>Kubernetes the Hardway</a>, not everything will be the most efficient, but it will be more "fun".</p>
+      <p>First piece of the puzzle is to build the Kubernetes AMIs for this we need packer and ansible. The repo is <a href='https://github.com/melvabout/fairground-machine-images'>fairground-machine-images</a></p>
     </Layout>
   )
 }
