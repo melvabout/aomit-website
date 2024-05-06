197840d7628c0c1de0320197d9e58493fc44bc93
Andrew Melvin <andy@aomit.co.uk>
2024-05-06 10:40:48 +0100

    Added the fairground images and invalidate the clountfront cache when deplying a new version of the site

--- gatsby/src/pages/fairground.js
+++ gatsby/src/pages/fairground.js
@@ -5,7 +5,9 @@ import Seo from '../components/seo'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Fairground">
-      <p>Coming ASAP.</p>
+      <p>Think sand or playground and make it bigger.</p>
+      <p>The goal is to build a mini web application that makes use of [FastAPI](https://fastapi.tiangolo.com/) and is deployed on to a monitored Kubernetes cluster in AWS. Inspired by [Kubernetes the Hardway](https://github.com/kelseyhightower/kubernetes-the-hard-way), not everything will be the most efficient, but it will be more "fun".</p>
+      <p>First piece of the puzzle is to build the Kubernetes AMIs for this we need packer and ansible. The repo is [fairground-machine-images](https://github.com/melvabout/fairground-machine-images)</p>
     </Layout>
   )
 }
